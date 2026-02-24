361a68e62e596e91572ac827de0252e46d998c08
om <omsingh8400@gmail.com>
2026-02-24 23:37:34 +0530

    added dynamic routing in product .js

--- microservices-app/product-service/server.js
+++ microservices-app/product-service/server.js
@@ -25,6 +25,11 @@ app.get("/api/products", async (req, res) => {
   const products = await Product.find()
   res.json(products)
 })
+app.get("/api/products/:id", async (req, res) => {
+  const product = await Product.findById(req.params.id)
+  if (!product) return res.status(404).json({ message: "Not found" })
+  res.json(product)
+})
 
 app.listen(5002, () => {
   console.log("Product Service running on port 5002")
