9ba6e715533ed4c1dfd1772ba886e386208f019e
om <omsingh8400@gmail.com>
2026-02-24 23:33:00 +0530

    added middleware

--- microservices-app/api-gateway/server.js
+++ microservices-app/api-gateway/server.js
@@ -1,10 +1,31 @@
 const express = require("express")
 const { createProxyMiddleware } = require("http-proxy-middleware")
 const cors = require("cors")
+const jwt = require("jsonwebtoken")
+
 
 const app = express()
 app.use(cors())
 
+
+
+
+
+
+
+function verifyToken(req, res, next) {
+  const token = req.headers.authorization?.split(" ")[1]
+
+  if (!token) return res.status(401).json({ message: "No token" })
+
+  try {
+    const decoded = jwt.verify(token, "supersecretkey")
+    req.user = decoded
+    next()
+  } catch (err) {
+    res.status(401).json({ message: "Invalid token" })
+  }
+}
 // Auth routes
 app.use(
   "/api/auth",
