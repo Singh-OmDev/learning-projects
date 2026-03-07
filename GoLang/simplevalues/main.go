af1373efbd5ebb757aa958a19de4f37b06e057d6
om <omsingh8400@gmail.com>
2026-03-07 17:38:21 +0530

    values in go

---
+++ GoLang/simplevalues/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// to print integer
+
+	fmt.Println(1);
+
+
+	// to print string
+	 fmt.Println("hello go lang");
+
+
+	  //boolean value
+
+	 fmt.Println(true);
+	 fmt.Println(false);
+
+	  //float values
+
+	   fmt.Println(3.14);
+
+	   
+}
