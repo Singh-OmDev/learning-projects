a2cadd51f45b47cc0bf6b39ce41658eb9e473c98
om <omsingh8400@gmail.com>
2026-03-14 21:37:10 +0530

    started auth

---
+++ auth/src/config/config.js
@@ -0,0 +1,15 @@
+import dotenv  from "dotenv"
+
+ dotenv.config();
+
+
+  if (!process.env.MONGO_URI){
+     throw new Error ("mongo_uri is not defined in environment variable");
+     
+  }
+
+  const config = {
+     MONGO_URI: process.env.MONGO_URI
+  }
+
+  export default  config;
---
+++ auth/src/routes/auth.route.js
@@ -0,0 +1,13 @@
+import  {Router} from "express"
+
+ const authRouter = Router ();
+
+
+
+
+ authRouter.post ("/register")
+
+
+  export default authRouter;
+
+
