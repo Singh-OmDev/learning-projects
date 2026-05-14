608207f347cf60001a317276f1b175a08f2b307b
om <omsingh8400@gmail.com>
2026-05-14 21:56:13 +0530

    s

--- crud api go/main.go
+++ crud api go/main.go
@@ -27,7 +27,7 @@ type Director struct {
 
 var movies []Movie
 
-// GET all movies
+// GET all movie
 func getMovies(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(movies)
