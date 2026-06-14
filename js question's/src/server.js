3683b054748a2b86d80770bb665b0683909bb360
om <omsingh8400@gmail.com>
2026-06-14 18:22:24 +0530

    apiss

---
+++ js question's/src/server.js
@@ -0,0 +1,82 @@
+import express from "express";
+
+const app = express ();
+ app.use (express.json());
+  app.use (express.urlencoded({extended:true}));
+
+
+ 
+
+  app.post ("/add-students", (req,res)=> {
+      const{ name, age, city, gender } = req.body;
+     res.send("student data added successfully");
+    console.log (`name: &{name} , age: & {age}, city: &{city}, genderbc: {gender}`); 
+  })
+   app.listen (8080, ()=> {
+     console.log ("server is running on port  8080");
+
+
+   })
+
+    app.get ("/students", (req, res)=> {
+    
+         
+        
+
+          const students = [
+             {
+                 name: "om",
+                  age: 22,
+                  city: "ballia",
+
+             },
+
+
+              {
+                 name : "rahul",
+                  age : 22,
+                   city: "delhi",
+                   gender: "male",
+              },
+
+               {
+
+               
+               name : "sumit",
+                age : 11,
+                 gender: "male",
+                  city :"up",
+               }
+          ]
+           
+        
+           
+           res.send (students);
+            res.status (200)
+            {
+                 console.log (students);
+            }
+            
+    })
+
+
+     app.put ("/update",  (req,res)=> {
+ const {name , age , city , gender} = req.body;
+   const updatedStudent = {
+    name:name,
+     age: age,
+      city: city,
+       gender: gender,
+
+   }
+    res.send (updatedStudent);
+    
+     console.log (`name: &{name}, age: {age}, city: &{city}, gender: &{gender}`);
+
+ console.log ("student data updated successfully hehehhehe ehhe") ;
+            
+
+
+     })
+
+     
--- react pratice/react-practice/src/App.jsx
+++ react pratice/react-practice/src/App.jsx
@@ -1,39 +1,53 @@
 import { useState } from "react";
 
 function App() {
-  // your code here
- const [count , setCount] = useState(0);
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const addTask = () => {
+    // your logic
+
+    if (task.trim() === "")return;
+
+     setTodos([...todos, task]);
+     setTask("");
 
 
- 
-  const increment = () => {
-     setCount(prev=> prev +1);
-      if (count >=10){
-         setCount (10);
-         
-      }
+
+  };
+
+  const deleteTask = (index) => {
 
 
-  }
-   const decrement = ()=> {
-     setCount(prev=> prev -1);
+    // your logic
 
-      if (count <=0){
-         setCount(0);
-
-      }
-
-   }
-  
-
+     const newTodos =[...todos];
+     newTodos.splice(index, 1);
+     setTodos(newTodos);
+     
+  };
 
   return (
     <div>
-      <h1>Count: {count}</h1>
-      
-      <button onClick={increment}> +</button>
-      <button onClick={decrement} > - </button>
-       
+      <h1>Todo App</h1>
+
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        placeholder="Enter task"
+      />
+
+      <button onClick={addTask}>Add</button>
+
+      <ul>
+        {todos.map((t, index) => (
+          <li key={index}>
+            {t}
+            <button onClick={() => deleteTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
