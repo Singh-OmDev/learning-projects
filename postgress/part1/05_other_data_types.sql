5e2add55edf5493acbc838a3e3e5bb948ef5d7b6
om <omsingh8400@gmail.com>
2026-07-09 00:13:32 +0530

    a

--- postgress/part1/05_other_data_types.sql
+++ postgress/part1/05_other_data_types.sql
@@ -1,3 +1,37 @@
-DROP TABLE  IF EXISTS basics.app_events;
+DROP TABLE IF EXISTS basics.app_events;
 
- 
+
+CREATE TABLE basics.app_events (
+
+    -- UUID
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+    event_name TEXT NOT NULL,
+
+    -- JSONB
+    metadata JSONB DEFAULT '{}'::jsonb,
+
+    -- timestamp
+    created_at TIMESTAMP DEFAULT NOW()
+
+);
+
+
+INSERT INTO basics.app_events (
+    event_name,
+    metadata
+)
+VALUES
+
+(
+    'sign_up',
+    '{"browser": "chrome"}'
+),
+
+(
+    'sign_in',
+    '{"browser": "brave"}'
+);
+
+
+SELECT * FROM basics.app_events;
