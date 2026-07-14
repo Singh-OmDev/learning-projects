a896bb2d70b1717abad8f1e12e6df78c29247d71
om <omsingh8400@gmail.com>
2026-07-14 09:19:12 +0530

    designing parking lot

---
+++ oops/parkinglot.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+ #include <vector>
+   #include <string>
+
+ using namespace std;
+  class Vehicle {
+
+    protected:
+     string  vechicleNumber;
+      string ownerName;
+
+
+
+       public:
+       
+
+  };
+
+
+  
+
+  int main (){
+
+
+
+  }
