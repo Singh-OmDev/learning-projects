bb8da5fbab69d1b185487f29ef2c67c65b72acfb
om <omsingh8400@gmail.com>
2026-07-14 11:42:47 +0530

    added inheritance

--- oops/parkinglot.cpp
+++ oops/parkinglot.cpp
@@ -10,14 +10,96 @@
       string ownerName;
 
 
-
        public:
-       
+        Vehicle ( string number , string owner) :
+         vechicleNumber (number), ownerName (owner){}
+             
+        
+  virtual void park (){
+     cout <<vechicleNumber <<"parked successfully"<<endl;
+
+  }
+
+   virtual void unpark (){
+     cout <<vechicleNumber<<"unparked successfully"<<endl;
+   }
+
+     virtual void displayInfo (){
+        cout <<"vehicle number:"<<vechicleNumber<<endl;
+        cout <<"owner name:"<<ownerName<<endl;
+
+     }
+
+
+     //getter
+      string getVehicleNumber (){
+         return vechicleNumber;
+
+
+      }
+
+       virtual ~Vehicle(){}
+
+
+
+  };
+
+ //----------------- car -------------//
+   class  Car : public Vehicle{
+     public:
+      Car ( string number , string owner)
+      : Vehicle (number  , owner){
+
+      }
+
+        void  displayInfo ()  override {
+             cout <<"\n---- Car----"<<endl;
+              cout <<"vehicle number :"<<vechicleNumber<<endl;
+             cout << "owner  name :"<<ownerName<<endl;
+              cout <<"type :car"<<endl;
+        }
+   };
+
+ class Bike : public Vehicle {
+     public:
+       Bike ( string number  , string owner): 
+        Vehicle ( number , owner){
+
+        }
+
+
+          void displayInfo () override {
+             cout <<"\n------ Bike ------"<<endl;
+             cout <<" vehicle number:"<< vechicleNumber<<endl;
+               cout <<"owner name"<<ownerName<<endl;
+                cout <<"type : bike"<<endl;
+
+
+          }
+ };
+
+
+
+ // truck  ---------
+  class Truck: public Vehicle {
+      public: 
+       Truck (string number , string owner): Vehicle ( number , owner){}
+
+
+        void displayInfo () override {
+             cout <<"\n------ truck -----"<<endl;
+              cout <<"vehicle number "<<vechicleNumber<<endl;
+               cout<< "owner "<< ownerName <<endl;
+                cout <<"type  truck "<<endl;
+
+        }
 
   };
 
 
-  
+
+
+
 
   int main (){
 
