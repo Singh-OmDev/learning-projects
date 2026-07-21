f76c26c913ad512d15eaa20469ad19f5297bcf5a
om <omsingh8400@gmail.com>
2026-07-21 10:26:44 +0530

    inner joins

---
+++ postgress/part3/03_one_to_many-relationship.sql
@@ -0,0 +1,21 @@
+--one parent  rows can have many child rows
+ -- one user can write many posts
+  -- but one post will always  belong to one user
+
+   -- user -- parent table 
+    --posts - child table
+
+    --posts.user_id -> user.id
+
+
+     --user.id is the original user id 
+      --posts.user_id stores that origial user id  inside the post table 
+      SELECT 
+       users.name AS author_name,
+        posts.title AS post_title,
+         posts.status 
+          FROM users
+           INNER JOIN posts
+            ON users.id = posts.user_id
+             ORDER BY users.name , posts.title;
+              
