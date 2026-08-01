a7b511e727e320f3959e47871251d604def6201c
om <omsingh8400@gmail.com>
2026-08-01 08:50:15 +0530

    generic array

---
+++ Typescripts1/genericArray.ts
@@ -0,0 +1,9 @@
+function firstElement <T> (arr : T []): T {
+     return arr[0];
+
+}
+
+ console.log (firstElement ([10, 20 , 33]));
+  console.log (firstElement (["hello", "typescript"]));
+
+   console.log (firstElement ([true, false  ]));
