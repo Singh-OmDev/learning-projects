4657eaf96fab68d01d576f90fb865d2ac0f23acd
om <omsingh8400@gmail.com>
2026-08-01 08:53:42 +0530

    pairs in generics

---
+++ Typescripts1/genericPair.ts
@@ -0,0 +1,10 @@
+function makePair <T , U > (first : T, second : U){
+     return {
+        first, 
+         second
+     };
+}
+
+ console.log (makePair ("om", 22));
+  console.log ( makePair (true , "admin"));
+  
