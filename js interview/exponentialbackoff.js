24fea8648f45cca267c95f69b538b8c8e8ead5f4
om <omsingh8400@gmail.com>
2026-08-02 14:57:52 +0530

    added exponential backoff to the promise

---
+++ js interview/exponentialbackoff.js
@@ -0,0 +1,13 @@
+ async  function  retry ( fn , retries = 3, delay = 500){
+     try {
+         return await fn ()
+     }
+      catch ( error){
+          if ( retries  === 0) 
+             throw error 
+             await new Promise  ( resolve => setTimeout ( resolve , delay))
+              return  retry ( fn  , retires -1 , delay *2);
+              
+      }
+     
+ }
