1e4d751ed577fc8338a0b57a9e881addfdf9e4c3
om <omsingh8400@gmail.com>
2026-08-03 10:23:06 +0530

    aadded get routes

--- tinyfile upload/tiny-file-api/src/controllers/file.controller.ts
+++ tinyfile upload/tiny-file-api/src/controllers/file.controller.ts
@@ -1,4 +1,8 @@
 import  {Request , Response} from 'express';
+
+ import {promises as fs} from 'fs';
+  import path from  "path";
+
  export const uploadFile = (
 
      req: Request ,
@@ -25,3 +29,37 @@ import  {Request , Response} from 'express';
       })
   };
 
+
+  export const getFiles = async (req: Request, res: Response) => {
+  try {
+    const uploadPath = path.join(
+      process.cwd(),
+      "src",
+      "storage",
+      "uploads"
+    );
+
+    // Read all filenames
+    const filenames = await fs.readdir(uploadPath);
+
+    // Get details for every file
+    const files = await Promise.all(
+      filenames.map(async (filename) => {
+        const filePath = path.join(uploadPath, filename);
+
+        const stats = await fs.stat(filePath);
+
+        return {
+          filename,
+          size: stats.size,
+        };
+      })
+    );
+
+    res.json(files);
+  } catch (error) {
+    res.status(500).json({
+      message: "Something went wrong",
+    });
+  }
+};
--- tinyfile upload/tiny-file-api/src/routes/file.routes.ts
+++ tinyfile upload/tiny-file-api/src/routes/file.routes.ts
@@ -2,12 +2,12 @@ import {Router} from "express";
 
 
  import upload from  "../middleware/upload";
-  import {uploadFile} from "../controllers/file.controller";
+  import {uploadFile , getFiles} from "../controllers/file.controller";
 
    const router = Router ();
 
     router.post ("/", upload.single ("file"),
  uploadFile);
+    router.get ("/", getFiles);
 
   export default router;
-  
