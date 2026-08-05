cdf8e1d82a196172ce5eb9c6556accec7cd1ad93
om <omsingh8400@gmail.com>
2026-08-05 19:35:39 +0530

    added login route

--- expense tracker/expense-tracker-backend/src/routes/auth.routes.js
+++ expense tracker/expense-tracker-backend/src/routes/auth.routes.js
@@ -3,7 +3,8 @@ const express = require ("express");
   
   const  authController = require ('../controllers/auth.controller');
 
-
+ router.post  ("/login", authController.login);
+ 
    router.post  ("/register", authController.register);
 
     module.exports = router;
