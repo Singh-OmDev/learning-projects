90f10256c8dfaa5d06583d6e6d960254b5952e1a
om <omsingh8400@gmail.com>
2026-08-18 09:58:28 +0530

    go basics

---
+++ go pratices/go-practice/main.go
@@ -0,0 +1,14 @@
+package main 
+
+
+ import "fmt" 
+
+	func main (){
+
+		 fmt.Println ("my name  is om");
+		  fmt.Println ("  i am learning go ");
+		   fmt.Println (" i want to become backend developer");
+
+		   
+	}
+ 
