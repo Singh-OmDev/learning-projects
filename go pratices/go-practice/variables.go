c7799822dae4fb92f1c098e63ff0b5716d518d0d
om <omsingh8400@gmail.com>
2026-08-18 11:34:57 +0530

    variables in go

---
+++ go pratices/go-practice/variables.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var name string = "om"
+var age int = 33
+var language string = "go"
+ var goal string = "I want to become a backend developer"
+
+func printVariables() {
+    fmt.Println("my name is", name)
+    fmt.Println("my age is", age)
+    fmt.Println("I am learning", language)
+    fmt.Println("my goal is", goal)
+}
---
+++ go pratices/go-practice/variables/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+var name string  = "om";
+var age = 22;
+
+ func printVariables (){
+		   height := 5.7;
+		    isStudent := true;
+			 age  = 33
+
+
+
+	  
+
+	   fmt.Println (" my  name is ", name);
+	    fmt.Println ("my age is " , age);
+		 fmt.Println ("my height is ", height );
+		  fmt.Println (" am i a student", isStudent);
+		   fmt.Println ("my age after birthday is"  ,age);
+		   
+
+ }
+	   
+ 
+
+func main() {
+	   printVariables()
+	  
+
+	   
+	 
+}
