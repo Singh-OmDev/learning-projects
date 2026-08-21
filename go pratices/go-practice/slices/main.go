55c0d4a3132849a04592c952250a5461aa746a8d
om <omsingh8400@gmail.com>
2026-08-21 12:04:04 +0530

    slices

---
+++ go pratices/go-practice/slices/main.go
@@ -0,0 +1,23 @@
+package main 
+ 
+ import "fmt"
+
+
+	 func slices(){
+		   numbers := [] int  {1, 2, 3, 4, 5}
+
+		    fmt.Println ("the numbers are  " , numbers)
+			 fmt.Println  ( " the  first number is " , numbers[0])
+			  	fmt.Println  ( " the  first number is " , numbers[1])
+				 fmt.Println  ( " the  first number is " , numbers[2])
+
+
+			
+
+
+	 }
+	    func main (){
+
+
+	 slices()
+  }
