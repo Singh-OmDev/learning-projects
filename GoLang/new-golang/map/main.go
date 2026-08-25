2540f84bd93f6a5803830b93d606a2f4c7303910
om <omsingh8400@gmail.com>
2026-08-25 12:10:01 +0530

    getting started with map

--- GoLang/new-golang/map/main.go
+++ GoLang/new-golang/map/main.go
@@ -3,8 +3,23 @@ package main
  import "fmt"
 
 
+
+
   func main (){
 
+	   ages := map [string] int {
+	 "john": 20,
+	  "om": 30,
+	   "sumit": 34,
 
-	
+  }
+
+
+	 fmt.Println (ages)
+
+	  fmt.Println ("john age is", ages["john"])
+
+	   fmt.Println ("om age is", ages["om"])
+	    fmt.Println ("sumit age is ", ages ["sumit"])
+
   }
