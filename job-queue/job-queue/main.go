55e45e0aad3b438c6c31597a307f8bc734792a9e
om <omsingh8400@gmail.com>
2026-09-22 11:43:08 +0530

    created worker

--- job-queue/job-queue/main.go
+++ job-queue/job-queue/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"net/http"
-
+	  "strconv"
+	   "fmt"
+	    "time"
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,9 +17,23 @@ type Job struct {
 
 var jobs []Job
 var nextID = 1
+ var  jobQueue  chan Job
+
+     func worker  ( ) {
+	 for job  := range  jobQueue  {
+		  fmt.Println ( "Processing job ID  :" , job.ID  ,  "Type :" , job.Type , "Payload :"  , job.Payload )
+		   time.Sleep ( 2 * time.Second) 
+		    fmt.Println  (  "completed job  ID : " , job.ID )
+	 }
+   }
 
 func main() {
 	r := gin.Default()
+	jobQueue = make(chan Job, 10)
+	  
+	 for  i := 1; i <=3; i++ {
+		 go worker ()
+	 }
 
 	// Home
 	r.GET("/", func(c *gin.Context) {
@@ -43,6 +59,7 @@ func main() {
 		job.Status = "pending"
 
 		jobs = append(jobs, job)
+		 jobQueue  <- job
 
 		c.JSON(http.StatusCreated, gin.H{
 			"message": "Job created successfully",
@@ -50,11 +67,104 @@ func main() {
 		})
 	})
 
+	 // Get all jobs
 	 r.GET("/jobs" , func  ( c  * gin.Context) {
 		 c.JSON (http.StatusOK , gin.H {
 			 "jobs": jobs,
 		 })
 	 })
+
+	  r.GET("/jobs/:id", func(c *gin.Context) {
+	id := c.Param("id")
+
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid job ID",
+		})
+		return
+	}
+
+	for _, job := range jobs {
+		if job.ID == idInt {
+			c.JSON(http.StatusOK, gin.H{
+				"job":     job,
+				"message": "Job retrieved successfully",
+			})
+			return
+		}
+	}
+
+	c.JSON(http.StatusNotFound, gin.H{
+		"error": "Job not found",
+	})
+})
+
+  r.DELETE( "/jobs/:id", func ( c * gin.Context) {
+		id := c.Param("id")
+
+		idInt, err := strconv.Atoi(id)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "Invalid job ID",
+			})
+			return
+		}
+
+		for i, job := range jobs {
+			if job.ID == idInt {
+				jobs = append(jobs[:i], jobs[i+1:]...)
+				c.JSON(http.StatusOK, gin.H{
+					"message": "Job deleted successfully",
+				})
+				return
+			}
+		}
+
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Job not found",
+		})
+  })
+  r.PUT("/jobs/:id", func(c *gin.Context) {
+	id := c.Param("id")
+
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid job ID",
+		})
+		return
+	}
+
+	var updatedJob Job
+
+	if err := c.ShouldBindJSON(&updatedJob); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	for i := range jobs {
+		if jobs[i].ID == idInt {
+
+			// Don't allow client to change ID or Status
+			jobs[i].Type = updatedJob.Type
+			jobs[i].Payload = updatedJob.Payload
+
+			c.JSON(http.StatusOK, gin.H{
+				"message": "Job updated successfully",
+				"job":     jobs[i],
+			})
+			return
+		}
+	}
+
+	c.JSON(http.StatusNotFound, gin.H{
+		"error": "Job not found",
+	})
+})
+
 
 	r.Run(":8080")
 }
